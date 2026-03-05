2c4a5104b06f8df9f9023454057ee42b828cda5d
Alejandro Mejia <alejomejiacuartas@outlook.com>
2026-03-05 12:25:05 +0100

    update page layout to finish desktop version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import { DesigualLogo } from "@/components/desigual-logo";
+import { ProductGallery } from "@/components/product-gallery";
+import { SizeGuideModal } from "@/components/size-guide-modal";
+
 export default function Home() {
   return (
-    <main>Hello world!</main>
+    <div className="grid grid-cols-2">
+      <div className="relative max-h-[calc(100vh-2.5rem)] mt-10 overflow-y-scroll">
+        <DesigualLogo
+          className="text-white h-10 absolute top-6 left-10 mix-blend-difference"
+        />
+        <ProductGallery />
+      </div>
+      <div className="mt-10 p-10">
+        <SizeGuideModal />
+      </div>
+    </div>
   );
 }
