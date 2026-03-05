64231ab20ce102081059f8b1a30a2f4280f4a9b9
Alejandro Mejia <alejomejiacuartas@outlook.com>
2026-03-05 12:21:59 +0100

    fix react and svg attribute issue

--- src/components/icons/icon-close.tsx
+++ src/components/icons/icon-close.tsx
@@ -3,7 +3,7 @@ import type { IconProps } from "./type"
 export function IconClose({ className }: IconProps) {
   return (
     <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" aria-hidden="true">
-      <path stroke-miterlimit="10" d="m4.5 4.5 15 15M4.5 19.5l15-15" />
+      <path strokeMiterlimit="10" d="m4.5 4.5 15 15M4.5 19.5l15-15" />
     </svg>
   )
 }
